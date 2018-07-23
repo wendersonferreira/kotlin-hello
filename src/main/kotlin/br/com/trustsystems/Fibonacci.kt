964bfd136ddc103d104816dfa61117da1705b20d
Wenderson Ferreira de Souza <wenderson.ferreira@gmail.com>
2018-07-23 20:11:07 -0300

    tailrec with fibonacci

---
+++ src/main/kotlin/br/com/trustsystems/Fibonacci.kt
@@ -0,0 +1,16 @@
+package br.com.trustsystems
+
+fun main(args: Array<String>) {
+    val fibonacciNumber = 10
+    print(fib(fibonacciNumber))
+}
+
+fun fib(n: Int): Int {
+    tailrec fun go(n: Int, a: Int, b: Int): Int {
+        return when (n) {
+            0 -> a
+            else -> go(n - 1, b, (a + b) % 100000)
+        }
+    }
+    return go(n % 1500000, 0, 1)
+}
